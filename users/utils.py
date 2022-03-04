161b0180abf5b1ed7d200077fd14946966b07ef3
Kimgwangil <kwangil0803@gmail.com>
2022-03-04 14:21:50 +0900

    Modify : SignUpView, Util

--- users/utils.py
+++ users/utils.py
@@ -7,13 +7,13 @@ from django.http import JsonResponse
 from users.models import User
 from my_settings  import SECRET_KEY, ALGORITHM
 
-def email_validation(email):
-    EMAIL_REGEX = re.match(r"^(\w+[+-_.]?\w?)+@([\w+-_.]+[.][\w+-_.]+)$", email)
-    return EMAIL_REGEX
+def validation_email(email):
+    EMAIL_REGEX = re.compile(r"^(\w+[+-_.]?\w?)+@([\w+-_.]+[.][\w+-_.]+)$")
+    return re.match(EMAIL_REGEX, email)
 
-def password_validation(password):
-    PASSWORD_REGEX = re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&?*])([a-zA-Z0-9!@#$%^&?*]){8,}$", password)
-    return PASSWORD_REGEX
+def validation_password(password):
+    PASSWORD_REGEX = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&?*])([a-zA-Z0-9!@#$%^&?*]){8,}$")
+    return re.match(PASSWORD_REGEX, password)
 
 def login_decorator(func):
     @wraps(func)
@@ -27,9 +27,9 @@ def login_decorator(func):
             return func(self, request, *args, **kwargs)
 
         except jwt.DecodeError:
-            return JsonResponse({ "MESSAGE" : "INVALID_TOKEN"}, status = 401)
+            return JsonResponse({ "message" : "INVALID_TOKEN"}, status = 401)
         
         except User.DoesNotExist:
-            return JsonResponse({ "MESSAGE" : "INVALID_USER"}, status = 401)
+            return JsonResponse({ "message" : "INVALID_USER"}, status = 401)
 
     return wrapper
--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.http  import JsonResponse
 from django.views import View
 
 from .models     import User
-from .utils import email_validation, password_validation
+from .utils import validation_email ,validation_password
 from my_settings import SECRET_KEY, ALGORITHM    
 
 
@@ -21,15 +21,21 @@ class SignUpView(View):
             gender_id       = data["gender_id"]
             password        = data["password"]
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            
+            if "" in data.values():
+                return JsonResponse({"message" : "EMPTY_DATA"}, status = 400)
 
-            if not email_validation(email):
-                return JsonResponse({"MESSAGE" : "INVALID_EMAIL"})
-            elif not password_validation(password):
-                return JsonResponse({"MESSAGE" : "INVALID_PASSWORD"})
+            if not validation_email(email):
+                return JsonResponse({"message" : "INVALID_EMAIL"}, status = 400)
+
+            if not validation_password(password):
+                return JsonResponse({"message" : "INVALID_PASSWORD"}, status = 400)
 
             if User.objects.filter(email = email).exists():
-                return JsonResponse({"MESSAGE" : "ALREADY_EXIST_EMAIL"})
+                return JsonResponse({"message" : "ALREADY_EXIST_EMAIL"}, status = 400)
             
+
+
             User.objects.create(
                 full_name    = full_name,
                 email        = email,
@@ -40,10 +46,10 @@ class SignUpView(View):
                 gender_id    = gender_id
             )
             
-            return JsonResponse({ "MESSAGE" : "SUCCESS" }, status = 201)
+            return JsonResponse({"message" : "SUCCESS"}, status = 201)
 
         except KeyError:
-            return JsonResponse({ "MESSAGE" : "KEYERROR" }, status = 400)
+            return JsonResponse({"message" : "KEYERROR"}, status = 400)
 
 
 class LogInView(View):
@@ -55,15 +61,15 @@ class LogInView(View):
             password = data["password"]
 
             if not User.objects.filter(email = email).exists():
-                return JsonResponse({ "MESSAGE" : "INVALID_USER" }, status = 400)
+                return JsonResponse({"message" : "INVALID_USER"}, status = 400)
 
             user         = User.objects.get(email = email)
             access_token = jwt.encode({'id' : user.id}, SECRET_KEY, ALGORITHM)
 
             if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                return JsonResponse({ "MESSAGE" : "INVALID_PASSWORD" }, status = 400)
+                return JsonResponse({"message" : "INVALID_PASSWORD"}, status = 400)
                             
-            return JsonResponse({ "MESSAGE" : "SUCCESS", "ACCESS_TOKEN" : access_token }, status = 201)
+            return JsonResponse({"message" : "SUCCESS", "ACCESS_TOKEN" : access_token}, status = 201)
 
         except KeyError:
-            return JsonResponse({ "MESSAGE" : "KEYERROR" }, status = 400)
+            return JsonResponse({"message" : "KEYERROR"}, status = 400)
