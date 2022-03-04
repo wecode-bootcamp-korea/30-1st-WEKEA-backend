0f7fe987e9f65d4b413a29bfa7fc766fb4d3e9de
Kimgwangil <kwangil0803@gmail.com>
2022-03-04 16:01:28 +0900

    Modify : login_decorator

--- users/utils.py
+++ users/utils.py
@@ -21,8 +21,9 @@ def login_decorator(func):
         try:
             access_token = request.headers.get('Authorization', None)
             payload      = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
-            user_id      = User.objects.get(id = payload["id"])
-
+            user         = User.objects.get(id = payload["id"])
+            request.user = user
+            
             return func(self, request, *args, **kwargs)
 
         except jwt.DecodeError:
