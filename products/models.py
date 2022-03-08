eb1a4a28bdd052629d19b4d554157bac6364b136
Kimgwangil <kwangil0803@gmail.com>
2022-03-08 11:48:09 +0900

    Modify : modified models

--- products/models.py
+++ products/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
 class Product(models.Model):
-    name         = models.CharField(max_length=50)
-    price        = models.DecimalField(max_digits=12, decimal_places=2)
-    description  = models.TextField()
-    sub_category = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
-    created_at   = models.DateTimeField(auto_now_add=True)
+    name          = models.CharField(max_length=50)
+    price         = models.DecimalField(max_digits=12, decimal_places=2)
+    description   = models.TextField()
+    sub_category  = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
+    discount      = models.ForeignKey('Discount', on_delete=models.CASCADE,default='')
+    created_at    = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'products'
 
+class Discount(models.Model):
+    rate=models.IntegerField(default=0)
+    type=models.CharField(max_length=200)
+    class Meta:
+        db_table = 'discounts'
 
 class Image(models.Model):
     image_url = models.CharField(max_length=200)
@@ -22,7 +28,9 @@ class Image(models.Model):
 class SubCategory(models.Model):
     name          = models.CharField(max_length=50)
     main_category = models.ForeignKey('MainCategory', on_delete=models.CASCADE)
-
+    description = models.TextField(default='')
+    image_url = models.CharField(max_length=200,null=True)
+    
     class Meta:
         db_table = 'sub_categories'
 
