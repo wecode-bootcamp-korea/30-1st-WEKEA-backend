79895078264c74f74094dda964c0dec7eb7e15b0
김산 <cloudhat17@gmail.com>
2022-03-10 22:44:56 +0900

    update 기능

--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
 from django.views               import View
 from django.http                import JsonResponse
-from .models                    import SubCategory,Product
+from .models                    import SubCategory,Product,DefaultFilteringOption
 from .utils                     import get_default_filtering_options, get_product_data,get_review_information, get_rating_average, get_discount_price, get_remaining_stock, get_option
-
+import pickle
+import ast
 
 class ProductListView(View):
     def get(self,request):
@@ -17,9 +18,9 @@ class ProductListView(View):
 
             result['product_data_list'] = get_product_data(sort_option,limit,offset,query_prams)
 
-            if filter_boolean == 'True': 
-                result['default_filtering_options'] = get_default_filtering_options(sub_category_id)
-            
+            byte_string                         = DefaultFilteringOption.objects.get(sub_category_id=sub_category_id).defualt_filtering_options
+            result['default_filtering_options'] = pickle.loads(ast.literal_eval(byte_string))
+
             SubCategory_object            = SubCategory.objects.get(id=sub_category_id)
             product_hierarchy             = SubCategory_object.main_category.name+"/"+SubCategory_object.name
             result['product_hierarchy']   = product_hierarchy 
@@ -29,13 +30,6 @@ class ProductListView(View):
 
         except KeyError:
             return JsonResponse({"message": "KEY ERROR"}, status= 400)
-
-
-from django.http  import JsonResponse
-from django.views import View
-
-from .models      import Product
-from .utils       import get_review_information, get_rating_average, get_discount_price, get_remaining_stock, get_option
 
 
 class ProductDetailView(View):
@@ -68,6 +62,24 @@ class ProductDetailView(View):
         except Product.DoesNotExist:
 
             return JsonResponse({"message" : "DOESNOTEXIST_PRODUCT_ID"}, status = 404)
-
             
-
+            
+class UpdateSubCategoryView(View):
+    def patch(self, request):
+           
+            if DefaultFilteringOption.objects.exists(): #기존에 데이터가 있을 경우 UPDATE
+                default_filtering_option_list=DefaultFilteringOption.objects.all()
+                for default_filtering_option_obejct in default_filtering_option_list:
+                    default_filtering_option_obejct.defualt_filtering_options = pickle.dumps(get_default_filtering_options(default_filtering_option_obejct.sub_category.id))
+                    default_filtering_option_obejct.save()
+            
+            else: #만약 기존에 DefaultFilteringOption가 비어있을 경우
+                sub_category_list=SubCategory.objects.all()
+                for sub_category_obeject in sub_category_list:
+                    sub_category_id           = sub_category_obeject.id
+                    defualt_filtering_options = get_default_filtering_options(sub_category_obeject.id)
+                    
+                    DefaultFilteringOption.objects.create(sub_category_id = sub_category_id,defualt_filtering_options=defualt_filtering_options)
+                
+            return JsonResponse({"message" : "SUCCESS"}, status = 200)
+       
